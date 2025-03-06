55071974d7d8a5bc2877dfec93102bd14786537a
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-03-06 17:04:24 -0300

    ADD entity User. Attributes and indexes created

---
+++ backend/src/entity/user.entity.ts
@@ -0,0 +1,32 @@
+"use strict";
+
+import { Entity, PrimaryGeneratedColumn, Column, Index } from "typeorm";
+
+@Entity("userauth") // Table name
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ type: "varchar", length: 255, nullable: false })
+    name!: string;
+
+    @Index("IDX_USER_RUT", { unique: true }) // Unique index for the rut column
+    @Column({ type: "varchar", length: 12, unique: true, nullable: false })
+    rut!: string;
+
+    @Index("IDX_USER_EMAIL", { unique: true }) // Unique index for the email column
+    @Column({ type: "varchar", length: 255, unique: true, nullable: false })
+    email!: string;
+
+    @Column({ type: "varchar", length: 50, nullable: false })
+    role!: string;
+
+    @Column({ type: "varchar", length: 255, nullable: false })
+    password!: string;
+
+    @Column({ type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP", nullable: false })
+    createAt!: Date;
+
+    @Column({ type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP", onUpdate:"CURRENT_TIMESTAMP", nullable: false })
+    updateAt!: Date;
+}
