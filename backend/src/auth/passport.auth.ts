28f1d61abbc38451d9d19d155e9d405a8fcbeda3
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-03-06 17:43:12 -0300

    ADD passport file

---
+++ backend/src/auth/passport.auth.ts
@@ -0,0 +1,34 @@
+"use strict";
+
+import passport from "passport";
+import { ExtractJwt, Strategy as JwtStrategy, StrategyOptions } from "passport-jwt";
+import { Repository } from "typeorm";
+import { ACCESS_TOKEN_SECRET } from "../config/configEnv.js";
+import { AppDataSource } from "../config/configDB.js";
+import { User } from "../entity/user.entity.js";
+
+const options: StrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: ACCESS_TOKEN_SECRET as string
+};
+
+passport.use(
+    new JwtStrategy(options, async(jwt_payload: { email: string }, done) => {
+        try {
+            const userRepository: Repository<User> = AppDataSource.getRepository(User);
+            const user = await userRepository.findOne({where: { email: jwt_payload.email }});
+
+            if (!user) {
+                return done(null, false);
+            }
+
+            return done(null, user);
+        } catch (error) {
+            return done(error, false);
+        }
+    })
+);
+
+export function passportJWTSetup() {
+    passport.initialize();
+}
