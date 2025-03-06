d77f96a5b46fe3af212edf55a2dbcb02bd1c18a3
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-03-06 17:01:57 -0300

    ADD config database and env files

---
+++ backend/src/config/configDB.ts
@@ -0,0 +1,31 @@
+"use strict";
+
+/* Import the required modules. */
+import { DataSource } from "typeorm";
+
+/* Import custom modules. */
+import { PORT, HOST, DATABASE, DB_USERNAME, DB_PASSWORD } from "./configEnv.js";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: HOST as string,
+    port: 5432,
+    username: DB_USERNAME as string,
+    password: DB_PASSWORD as string,
+    database: DATABASE as string,
+    synchronize: true,
+    logging: false,
+    entities: [
+        "src/entity/**/*.ts"
+    ],
+});
+
+export async function connectDB(): Promise<void> {
+    try {
+        await AppDataSource.initialize();
+        console.log("=> Database connected successfully.");
+    } catch (error) {
+        console.error("=> Error connecting to the database: ", error);
+        process.exit(1);
+    }
+}
---
+++ backend/src/config/configEnv.ts
@@ -0,0 +1,20 @@
+"use strict";
+
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __envPath = path.resolve(__dirname, ".env");
+
+dotenv.config({ path: __envPath });
+
+// Export the configuration object.
+export const PORT: string | undefined = process.env.PORT;
+export const HOST: string | undefined = process.env.HOST;
+export const DATABASE: string | undefined = process.env.DATABASE;
+export const DB_USERNAME: string | undefined = process.env.USERNAME;
+export const DB_PASSWORD: string | undefined = process.env.PASSWORD;
+export const ACCESS_TOKEN_SECRET: string | undefined = process.env.ACCESS_TOKEN_SECRET;
+export const cookieKey: string | undefined = process.env.cookieKey;
