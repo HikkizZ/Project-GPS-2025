a4d3abb8bb03526de8c7afdd32fa2a7abe08a63d
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-03-06 17:57:49 -0300

    ADD Server file

---
+++ backend/src/server.ts
@@ -0,0 +1,80 @@
+"use strict";
+
+/* Import the required modules. */
+import express, { json, urlencoded, Application} from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import passport from "passport";
+
+import indexRoutes from "./routes/index.routes.js";
+import { connectDB } from "./config/configDB.js";
+import { cookieKey, PORT, HOST } from "./config/configEnv.js";
+import { passportJWTSetup } from "./auth/passport.auth.js";
+import { createUsers } from "./utils/initialSetup.js";
+
+async function setupServer(): Promise<void> {
+    try {
+        const app: Application = express();
+
+        app.disable("x-powered-by");
+
+        app.use(cors({
+            origin: true,
+            credentials: true
+        }));
+        
+        app.use(urlencoded({
+            extended: true,
+            limit: "1mb"
+        }));
+
+        app.use(json({
+            limit: "1mb"
+        }));
+
+        app.use(cookieParser());
+
+        app.use(morgan("dev"));
+
+        app.use(session({
+            secret: cookieKey as string,
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                secure: false,
+                httpOnly: true,
+                sameSite: "strict",
+            }
+        }));
+
+        app.use(passport.initialize());
+
+        app.use(passport.session());
+
+        passportJWTSetup();
+
+        app.use("/api", indexRoutes);
+
+        app.listen(PORT, () => {
+            console.log(`=> Server running on http://${HOST}:${PORT}/api`);
+        });
+    } catch (error) {
+        console.error("=> Error starting the server: -> setupServer(). Error: ", error);
+    }
+}
+
+async function setupAPI(): Promise<void> {
+    try {
+        await connectDB();
+        await setupServer();
+        await createUsers();
+    } catch (error) {
+        console.error("=> Error setting up the API: -> setupAPI(). Error: ", error);
+    }
+}
+
+setupAPI()
+    .then(() => console.log("=> API started successfully."))
+    .catch((error) => console.error("=> Error starting the API: ", error));
