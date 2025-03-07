f630e72d382c4bc34bf7296a4a94a61b9a3c7fdd
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-03-07 15:43:24 -0300

    MOD types.d.ts
    feat(types): define taskStatus and taskPriority with allowed values

--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,2 +1,8 @@
 /* Role Types */
 export type userRole = 'Admin' | 'Profesor' | 'Alumno' | 'Encargado' |'Usuario' | 'Invitado';
+
+/* Priority Types */
+export type taskPriority = 'Alta' | 'Media' | 'Baja';
+
+/* Status Types */
+export type taskStatus = 'Pendiente' | 'En Proceso' | 'Finalizada' | 'Cancelada';
