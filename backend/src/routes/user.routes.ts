aeedbc3fe11d0422d81af228890cb364d150c17c
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-05-14 01:06:57 -0400

    feat(user.router): protect all routes with authenticateJWT middleware
    AuthenticateJWT was added to restrict access to user operations to authenticated users only.

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+
+import { authenticateJWT } from "../middlewares/authentication.middleware.js";
+
 import { getUser, getUsers, updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router: Router = Router();
+
+router.use(authenticateJWT);
 
 router
     .get("/", getUser)
