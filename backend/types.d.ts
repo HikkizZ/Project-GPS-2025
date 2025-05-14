1ffd31eb40117714616515f7e1f8818bb379a436
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-05-14 01:30:38 -0400

    type: The types of roles for the project are standardized.

--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,11 +1,5 @@
 /* Role Types */
-export type userRole = 'Admin' | 'Profesor' | 'Alumno' | 'Encargado' |'Usuario' | 'Invitado';
-
-/* Priority Types */
-export type taskPriority = 'Alta' | 'Media' | 'Baja';
-
-/* Status Types */
-export type taskStatus = 'Pendiente' | 'En Proceso' | 'Finalizada' | 'Cancelada';
+export type userRole = 'Administrador' | 'Usuario' | 'RecursosHumanos' | 'Gerencia' |'Ventas' | 'Arriendo' | 'Finanzas';
 
 /* userResponse Interface */
 export interface UserResponse {
