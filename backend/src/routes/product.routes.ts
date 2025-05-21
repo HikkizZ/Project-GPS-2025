b6d1d14241be377a968c65e60b548531db6a38b9
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-05-21 18:58:30 -0400

    add: creación de las rutas para productos
    Posiblemente requiera corrección por el uso de parametros

---
+++ backend/src/routes/product.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import {
+    getProducts,
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+} from "../controllers/inventory/product.controller.js";
+
+import { authenticateJWT } from "../middlewares/authentication.middleware.js";
+
+const router: Router = Router();
+
+router.use(authenticateJWT);
+
+router
+    .get("/", getProducts)
+    .get("/", getProduct)
+    .post("/", createProduct)
+    .patch("/", updateProduct)
+    .delete("/", deleteProduct);
