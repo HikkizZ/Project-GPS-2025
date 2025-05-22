633842a8b8a52e1d7c4bf401ce1e0f2aecb903b2
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-05-22 16:26:21 -0400

    feat: se agregan las rutas de productos

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 import  authRoutes from './auth.routes.js';
 import userRoutes from './user.routes.js';
+import productRoutes from './product.routes.js';
 
 const router: Router = Router();
 
 /* Test route */
-router.get('/', (req, res) => {
+router.get('/', (_req, res) => {
   res.send('Hello World');
 });
 
 /* Here are the routes */
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
+router.use('/product', productRoutes);
 
 export default router;
