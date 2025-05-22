eeb334021f60df6e7628686aa9d3ae093b0a6013
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-05-21 20:56:30 -0400

    fix: index.ts será para tipados especificos. Aquí coloqué por ahora el tipado de product
    Si funciona, no lo toques

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,6 +1,3 @@
-/* Role Types */
-export type userRole = 'Administrador' | 'Usuario' | 'RecursosHumanos' | 'Gerencia' |'Ventas' | 'Arriendo' | 'Finanzas';
-
 /* Product Types */
 export enum ProductType {
     BASE_ESTABILIZADA = 'BASE_ESTABILIZADA',
@@ -12,46 +9,6 @@ export enum ProductType {
     RIPIO = 'RIPIO',
     RELLENO = 'RELLENO'
 }
-
-/* userResponse Interface */
-export interface UserResponse {
-    id: number;
-    name: string;
-    rut: string;
-    email: string;
-    role: string;
-    createAt: string;
-    updateAt: string; 
-}
-
-import { User } from '../entity/user.entity.js';
-import { Request } from 'express';
-
-declare module 'express' {
-    export interface Request {
-        user?: User;
-    }
-}
-
-/* User Services Types */
-export type ServiceResponse<T> = [T | null, string | { message: string} | null];
-
-export type QueryParams = {
-    id?: number;
-    email?: string;
-    rut?: string;
-}
-
-export type UpdateUserData = {
-    name?: string;
-    email?: string;
-    rut?: string;
-    password?: string;
-    newPassword?: string;
-    role?: userRole;
-}
-
-export type SafeUser = Omit<User, 'password'>; // Exclude the password field from the User type
 
 export type CreateProductDTO = {
   product: ProductType;
