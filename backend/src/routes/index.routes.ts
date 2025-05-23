7d3320ab19ea9ef9ccd45cb53477a4e8d5a514c0
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-05-22 20:26:13 -0400

    mod: se añadió la ruta /supplier al enrutador principal

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -3,6 +3,7 @@ import  authRoutes from './auth.routes.js';
 import userRoutes from './user.routes.js';
 import productRoutes from './product.routes.js';
 import customerRoutes from './customer.routes.js';
+import supplierRoutes from './supplier.routes.js';
 
 const router: Router = Router();
 
@@ -16,5 +17,6 @@ router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/product', productRoutes);
 router.use('/customer', customerRoutes);
+router.use('/supplier', supplierRoutes);
 
 export default router;
