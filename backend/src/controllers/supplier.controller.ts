409ad68ae65278b9a3e71abf7a62f69f308b7021
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-05-22 20:25:06 -0400

    add: Se implementan los controladores de proveedores con validación y manejo de errores
    Se reutilizan los validadores person.validation.ts utilizados en clientes

---
+++ backend/src/controllers/supplier.controller.ts
@@ -0,0 +1,160 @@
+import { Response, Request } from 'express';
+import {
+    getAllSuppliersService,
+    getSupplierService,
+    createSupplierService,
+    updateSupplierService,
+    deleteSupplierService,
+} from '../services/supplier.service.js';
+
+import { handleSuccess, handleErrorClient, handleErrorServer } from '../handlers/responseHandlers.js';
+
+import { personQueryValidation, personBodyValidation } from '../validations/person.validation.js';
+
+export async function getSuppliers(_req: Request, res: Response): Promise<void> {
+    try {
+        const [suppliers, error] = await getAllSuppliersService();
+
+        if (error) {
+            handleErrorServer(res, 404, typeof error === 'string' ? error : error.message);
+            return;
+        }
+
+        if (!suppliers || suppliers.length === 0) {
+            handleErrorClient(res, 404, "No se encontraron proveedoress.");
+            return;
+        }
+
+        handleSuccess(res, 200, "Proveedores obtenidos correctamente", suppliers!);
+    } catch (error) {
+        handleErrorServer(res, 500, (error as Error).message);
+    }
+}
+
+export async function getSupplier(req: Request, res: Response): Promise<void> {
+    try {
+        const rut = req.query.rut as string  | undefined;
+        const email = req.query.email as string | undefined;
+        const id = req.query.id ? Number(req.query.id) : undefined;
+
+        const { error } = personQueryValidation.validate({ rut, email, id });
+
+        if (error) {
+            handleErrorClient(res, 400, error.message);
+            return;
+        }
+
+        const [supplier, errorSupplier] = await getSupplierService({ rut, email, id });
+
+        if (errorSupplier) {
+            const message = typeof errorSupplier === 'string' ? errorSupplier : errorSupplier.message;
+            handleErrorClient(res, 404, message);
+            return;
+        }
+
+        if (!supplier) {
+            handleErrorClient(res, 404, "No se encontró el proveedor.");
+            return;
+        }
+
+        handleSuccess(res, 200, 'Proveedor encontrado.', supplier);
+    } catch (error) {
+        handleErrorServer(res, 500, (error as Error).message);
+    }
+}
+
+export const createSupplier = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { error } = personBodyValidation.validate(req.body);
+
+        if (error) {
+            handleErrorClient(res, 400, error.message);
+            return;
+        }
+
+        const [supplier, errorSupplier] = await createSupplierService(req.body);
+
+        if (errorSupplier) {
+            handleErrorClient(res, 400, typeof errorSupplier === 'string' ? errorSupplier : errorSupplier.message);
+            return;
+        }
+
+        if (!supplier) {
+            handleErrorServer(res, 500, "Error inesperado: No se pudo crear el proveedor.");
+            return;
+        }
+
+        handleSuccess(res, 201, "Proveedor creado correctamente.", supplier);
+    } catch (error) {
+        handleErrorServer(res, 500, (error as Error).message);
+    }
+}
+
+export async function updateSupplier(req: Request, res: Response): Promise<void> {
+    try {
+        const id = req.query.id ? Number(req.query.id) : undefined;
+        const rut = req.query.rut as string | undefined;
+        const email = req.query.email as string | undefined;
+
+        const { error: queryError } = personQueryValidation.validate({ id, rut, email });
+
+        if (queryError) {
+            handleErrorClient(res, 400, queryError.message);
+            return;
+        }
+
+        const { error: bodyError } = personBodyValidation.validate(req.body);
+
+        if (bodyError) {
+            handleErrorClient(res, 400, bodyError.message);
+            return;
+        }
+
+        const [supplier, errorSupplier] = await updateSupplierService({ id, rut, email }, req.body);
+
+        if (errorSupplier) {
+            handleErrorClient(res, 404, typeof errorSupplier === 'string' ? errorSupplier : errorSupplier.message);
+            return;
+        }
+
+        if (!supplier) {
+            handleErrorClient(res, 404, "No se encontró el proveedor.");
+            return;
+        }
+
+        handleSuccess(res, 200, "Proveedor actualizado correctamente.", supplier);
+    } catch (error) {
+        handleErrorServer(res, 500, (error as Error).message);
+    }
+}
+
+export async function deleteSupplier(req: Request, res: Response): Promise<void> {
+    try {
+        const id = req.query.id ? Number(req.query.id) : undefined;
+        const rut = req.query.rut as string | undefined;
+        const email = req.query.email as string | undefined;
+
+        const { error } = personQueryValidation.validate({ id, rut, email });
+
+        if (error) {
+            handleErrorClient(res, 400, error.message);
+            return;
+        }
+
+        const [deletedSupplier, errorSupplier] = await deleteSupplierService({ id, rut, email });
+
+        if (errorSupplier) {
+            handleErrorClient(res, 404, typeof errorSupplier === 'string' ? errorSupplier : errorSupplier.message);
+            return;
+        }
+
+        if (!deletedSupplier) {
+            handleErrorClient(res, 404, "No se encontró el proveedor.");
+            return;
+        }
+
+        handleSuccess(res, 200, "Proveedor eliminado correctamente.", deletedSupplier);
+    } catch (error) {
+        handleErrorServer(res, 500, (error as Error).message);
+    }
+}
