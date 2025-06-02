b59428b2b25094831b4665e476babf7a54b7c9ac
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-06-01 21:39:41 -0400

    mod: se añade al enrutamiento rutas para la salida de inventario (/inventario-exit) y para el inventario general (/inventory)

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -5,6 +5,8 @@ import productRoutes from './product.routes.js';
 import customerRoutes from './customer.routes.js';
 import supplierRoutes from './supplier.routes.js';
 import inventoryEntryRoutes from './inventoryEntry.routes.js';
+import inventoryExitRoutes from './inventoryExit.routes.js';
+import inventoryRoutes from './inventory.routes.js';
 
 const router: Router = Router();
 
@@ -20,5 +22,7 @@ router.use('/product', productRoutes);
 router.use('/customer', customerRoutes);
 router.use('/supplier', supplierRoutes);
 router.use('/inventory-entry', inventoryEntryRoutes);
+router.use('/inventory-exit', inventoryExitRoutes);
+router.use('/inventory', inventoryRoutes);
 
 export default router;
