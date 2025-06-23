9d15df6bbfa0bf805e0fdebaa783beb8bbeae37c
Javier Santander <javier.santander2101@alumnos.ubiobio.cl>
2025-06-22 22:30:57 -0400

    Creación de entidad intermedia entre bono y trabajador asignarBono

---
+++ backend/src/entity/recursosHumanos/Remuneraciones/asignarBono.entity.ts
@@ -0,0 +1,31 @@
+
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    JoinColumn
+} from "typeorm";
+import { Trabajador } from "../trabajador.entity.js";
+import { Bono } from "./Bono.entity.js";
+
+@Entity("asignar_bono")
+export class AsignarBono {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @ManyToOne(() => Trabajador, trabajador => trabajador.asignaciones, { nullable: true })
+    trabajador!: Trabajador;
+
+    @ManyToOne(() => Bono, bono => bono.asignaciones, { nullable: false })
+    bono!: Bono;
+
+    @Column({ type: "date", nullable: false, default: () => "CURRENT_DATE" })
+    fechaEntrega!: Date;
+
+    @Column({ type: "boolean", default: true })
+    activo!: boolean;
+
+    @Column({ type: "text", nullable: true })
+    observaciones?: string;
+}
