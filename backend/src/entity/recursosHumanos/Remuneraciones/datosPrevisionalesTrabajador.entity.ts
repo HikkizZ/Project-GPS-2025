7539bc483da5557219a1f05597501dd63ac0bec3
Javier Santander <javier.santander2101@alumnos.ubiobio.cl>
2025-06-22 22:26:57 -0400

    MOD: Cambio a sub carpeta de remuneraciones para mayor comprensión de divisiones de areas

---
+++ backend/src/entity/recursosHumanos/Remuneraciones/datosPrevisionalesTrabajador.entity.ts
@@ -0,0 +1,32 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    OneToOne,
+    JoinColumn
+} from "typeorm";
+import { Trabajador } from "../trabajador.entity.js";
+import { PrevisionAFP } from "./previsionAFP.entity.js";
+import { PrevisionSalud } from "./previsionSalud.entity.js";
+
+@Entity("datosPrevisionalesTrabajadores")
+export class DatosPrevisionalesTrabajador {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @OneToOne(() => Trabajador, trabajador => trabajador.datosPrevisionales, { nullable: false })
+    @JoinColumn({ name: "datosPrevisionales" })
+    trabajador!: Trabajador;
+
+    @OneToOne(() => PrevisionAFP, previsionAFP => previsionAFP.id, { nullable: false })
+    @JoinColumn({ name: "idAFP" })
+    idAFP!: string;
+
+    @OneToOne(() => PrevisionSalud, previsionSalud => previsionSalud.id, { nullable: false })
+    @JoinColumn({ name: "idSalud" })
+    idSalud!: string;
+
+    @Column({ type: "varchar", length: 50, nullable: true })
+    seguroCesantia!: string;
+
+}
