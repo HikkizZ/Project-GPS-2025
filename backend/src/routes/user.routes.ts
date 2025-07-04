9e533d7973fd9dcebc7a36bcfd262ea7c56982cb
Pygc <patricia.gonzalez2001@alumnos.ubiobio.cl>
2025-07-03 21:48:56 -0400

    roles permitidos backend de userr outes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,16 +1,20 @@
 import { Router, Request, Response } from "express";
 import { authenticateJWT } from "../middlewares/authentication.middleware.js";
+import { verifyRole } from "../middlewares/authorization.middleware.js";
 import { getUsers, updateUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
+// Solo SuperAdministrador, Administrador y RecursosHumanos pueden acceder a estas rutas
+const allowedRoles = ["SuperAdministrador", "Administrador", "RecursosHumanos"];
+
 // Ruta única para listar y buscar usuarios (con o sin filtros)
-router.get("/", authenticateJWT, async (req: Request, res: Response) => {
+router.get("/", authenticateJWT, verifyRole(allowedRoles), async (req: Request, res: Response) => {
     await getUsers(req, res);
 });
 
 // Actualizar usuario por ID
-router.put("/:id", authenticateJWT, async (req: Request, res: Response) => {
+router.put("/:id", authenticateJWT, verifyRole(allowedRoles), async (req: Request, res: Response) => {
     await updateUser(req, res);
 });
 
