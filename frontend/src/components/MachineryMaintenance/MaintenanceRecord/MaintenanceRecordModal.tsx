68255d2541de9d4bdff8227401a8b57bcabdb3f8
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2025-07-18 01:39:20 -0400

    Creación de modal MaintenaceRecord

---
+++ frontend/src/components/MachineryMaintenance/MaintenanceRecord/MaintenanceRecordModal.tsx
@@ -0,0 +1,28 @@
+// src/components/mantencionMaquinaria/MaintenanceRecordModal.tsx
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import MaintenanceRecordForm from './MaintenanceRecordForm';
+import { CreateMaintenanceRecordData } from '@/types/machinaryMaintenance/maintenanceRecord.types';
+
+interface Props {
+  show: boolean;
+  onHide: () => void;
+  onSubmit: (data: CreateMaintenanceRecordData) => void;
+  initialData?: Partial<CreateMaintenanceRecordData>;
+  loading?: boolean;
+}
+
+const MaintenanceRecordModal: React.FC<Props> = ({ show, onHide, onSubmit, initialData, loading }) => {
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{initialData ? 'Editar Mantención' : 'Registrar Mantención'}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <MaintenanceRecordForm initialData={initialData} onSubmit={onSubmit} loading={loading} />
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default MaintenanceRecordModal;
