3bd468a6f2fd784461a1955d4dc409c4cbcda2d6
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-07-21 03:58:12 -0400

    add: Tipado para clientes

--- frontend/src/types/stakeholders/customer.types.ts
+++ frontend/src/types/stakeholders/customer.types.ts
@@ -45,3 +45,9 @@ export interface PaginatedCustomers {
     limit: number;
     totalPages: number;
 }
+
+export interface ApiResponse<T = any> {
+  success: boolean;
+  message: string;
+  data?: T;
+}
