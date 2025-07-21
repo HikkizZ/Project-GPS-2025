49c0fa066f00bdade2ad3c7c0caad67054ce40ed
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-07-20 22:28:20 -0400

    mod: se cambian servicios de update y delete de query params a path params

--- backend/src/services/stakeholders/supplier.service.ts
+++ backend/src/services/stakeholders/supplier.service.ts
@@ -23,6 +23,7 @@ export async function getAllSuppliersService(): Promise<ServiceResponse<Supplier
 export async function getSupplierService(query: QueryParams): Promise<ServiceResponse<Supplier>> {
     try {
         const { id, rut, email } = query;
+        console.log("Query parameters for supplier search:", query);
 
         const rutFormatted = rut ? formatRut(rut) : undefined;
 
@@ -64,15 +65,12 @@ export async function createSupplierService(supplierData: CreateSupplierDTO): Pr
     }
 }
 
-export async function updateSupplierService(query: QueryParams, supplierData: UpdateSupplierDTO): Promise<ServiceResponse<Supplier>> {
+export async function updateSupplierService(query: { id: number }, supplierData: UpdateSupplierDTO): Promise<ServiceResponse<Supplier>> {
     try {
-        const { id, rut, email } = query;
-
-        const rutFormatted = rut ? formatRut(rut) : undefined;
+        const { id } = query;
 
         const supplierRepository = AppDataSource.getRepository(Supplier);
-
-        const supplierFound = await supplierRepository.findOne({ where: [{ id }, { rut: rutFormatted }, { email }] });
+        const supplierFound = await supplierRepository.findOne({ where: { id } });
 
         if (!supplierFound) return [null, "El proveedor no existe."];
 
@@ -81,7 +79,7 @@ export async function updateSupplierService(query: QueryParams, supplierData: Up
         const updatedSupplier = await supplierRepository.save({
             ...supplierFound,
             ...supplierData,
-            rut: formattedUpdateRut ?? supplierFound.rut
+            rut: formattedUpdateRut ?? supplierFound.rut,
         });
 
         return [updatedSupplier, null];
@@ -91,15 +89,13 @@ export async function updateSupplierService(query: QueryParams, supplierData: Up
     }
 }
 
-export async function deleteSupplierService(query: QueryParams): Promise<ServiceResponse<Supplier>> {
-    try {
-        const { id, rut, email } = query;
 
-        const rutFormatted = rut ? formatRut(rut) : undefined;
+export async function deleteSupplierService(query: { id: number }): Promise<ServiceResponse<Supplier>> {
+    try {
+        const { id } = query;
 
         const supplierRepository = AppDataSource.getRepository(Supplier);
-
-        const supplierFound = await supplierRepository.findOne({ where: [{ id }, { rut: rutFormatted }, { email }] });
+        const supplierFound = await supplierRepository.findOne({ where: { id } });
 
         if (!supplierFound) return [null, "El proveedor no existe."];
 
