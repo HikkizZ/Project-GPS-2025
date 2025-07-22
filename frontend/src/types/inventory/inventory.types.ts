09e89d4faf99bc0fca44b0fb8a15efd6e2a59f0b
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-07-21 21:03:28 -0400

    mod: se agregan tipos para inventario de salida

--- frontend/src/types/inventory/inventory.types.ts
+++ frontend/src/types/inventory/inventory.types.ts
@@ -29,6 +29,43 @@ export interface InventoryEntry {
   }[];
 }
 
+export interface InventoryExitDetailData {
+  productId: number
+  quantity: number
+}
+
+export interface CreateInventoryExitData {
+  customerRut: string
+  details: InventoryExitDetailData[]
+}
+
+export interface InventoryExit {
+  id: number
+  exitDate?: string
+  customer: {
+    // Considerar usar el tipo Customer completo si se necesitan más propiedades
+    id: number
+    name: string
+    rut: string
+    address: string
+    phone: string
+    email: string
+  }
+  details: {
+    id: number
+    product: {
+      // Considerar usar el tipo Product completo si se necesitan más propiedades
+      id: number
+      product: string
+      salePrice: number
+      isActive: boolean // Añadido según tu ejemplo de backend
+    }
+    quantity: number
+    salePrice: number // Precio de venta del producto en el momento de la salida
+    totalPrice: number
+  }[]
+}
+
 export interface ApiResponse<T = any> {
   success: boolean;
   message: string;
